b7837fe639f4b47cdecc54da77505acbb09a7c6b
Karim Osman <kareemosman199@gmail.com>
2024-08-29 23:44:39 +0300

    add MultiClientChatClass

--- WebServerCpp/main.cpp
+++ WebServerCpp/main.cpp
@@ -1,8 +1,18 @@
-#include "TcpListener.h"
+#include "MultiClientChat.h"
 
 //using namespace std;
 
 void main()
 {
+
+	MultiClientChat mcc("0.0.0.0", 54000);
+
+	if (mcc.init() != 0) {
+		fprintf(stdout, "end\n");
+		return;
+	}
+
+	mcc.run();
+
 	system("pause");
 }
