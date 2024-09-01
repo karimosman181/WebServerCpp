b7f0257e5a8974227c9088dd6fc06fbe6c4d0da1
Karim Osman <kareemosman199@gmail.com>
2024-09-01 21:27:24 +0300

    parse header

--- WebServerCpp/WebServer.cpp
+++ WebServerCpp/WebServer.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <sstream>
+#include <istream>
+#include <iterator>
 #include <fstream>
 #include <iostream>
+#include <vector>
 #include "WebServer.h"
 
 // handler for when a message is received from client
@@ -10,19 +13,35 @@ void WebServer::onMessageReceived(int clientSocket, const char* msg, int length)
 	// GET /index.html  HTTP/1.1
 
 	// Parse out the document requested
-	// Open the document in the local file system
-	std::ifstream f(".\\www\\index.html");
+	std::istringstream iss(msg);
+	std::vector<std::string> parsed((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+
+
+
 	std::string content = "<h1>404 Not Found !<h1>";
 	int code = 404;
 	std::string codeMessage = "Not Found";
+	std::string path = "\\index.html";
 	
-	if (f.good())
+	
+	if (parsed.size() >= 3 && parsed[0] == "GET")
 	{
-		code = 200;
-		codeMessage = "OK";
 
-		std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-		content = str;
+		if (parsed[1] != "/")
+			path = parsed[1];
+
+		// Open the document in the local file system
+		std::ifstream f(".\\www" + path);
+
+
+		if (f.good())
+		{
+			code = 200;
+			codeMessage = "OK";
+
+			std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+			content = str;
+		}
 	}
 	
 	// write the document back to the client
