8f586e53305318e376899f36f4089e504135c090
Karim Osman <kareemosman199@gmail.com>
2024-09-11 22:00:40 +0300

    add functions

--- WebServerCpp/Tokenizer.h
+++ WebServerCpp/Tokenizer.h
@@ -18,4 +18,19 @@ enum class TOKEN
 class Tokenizer
 {
 
+public:
+    //gets the next character after skipping all the whitespace and newlines.
+    auto getWithoutWhiteSpace();
+
+    // getToken, the logic is :
+    // For characters like ‘[’, ‘, ’, ‘]’, ‘{ ’, ‘ }’, map it to the corressponding enum type 
+    // for string, keep consuming characters till ‘"’ is reached 
+    // for numbers, keep consuming characters consecutive number characters.
+    // store the string / number in the “value” field of the token.
+    auto getToken();
+
+    auto hasMoreTokens();
+
+    // goes back to the previous token. 
+    void rollBackToken();
 };
