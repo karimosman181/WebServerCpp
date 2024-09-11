22fb540dab487076292d08b3ce1096ee86606bbb
Karim Osman <kareemosman199@gmail.com>
2024-09-11 21:51:51 +0300

    add tokenizer for the json parser

---
+++ WebServerCpp/Tokenizer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+enum class TOKEN
+{
+    CURLY_OPEN,
+    CURLY_CLOSE,
+    COLON,
+    STRING,
+    NUMBER,
+    ARRAY_OPEN,
+    ARRAY_CLOSE,
+    COMMA,
+    BOOLEAN,
+    NULL_TYPE
+};
+
+class Tokenizer
+{
+
+};
