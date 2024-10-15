3e3d45309c73a7547829b413dbfdbc2b0dfae821
Karim Osman <kareemosman199@gmail.com>
2024-10-15 20:22:08 +0300

    add MetaData struct

--- WebServerCpp/src/routing/Response.h
+++ WebServerCpp/src/routing/Response.h
@@ -3,12 +3,31 @@
 #include "json.h"
 
 
+struct MetaData {
+    std::string title;
+    std::string description;
+    std::vector<std::string> keywords; // Relevant keywords
+    std::string viewport = "width=device-width, initial-scale=1.0"; // Viewport meta tag
+    std::string canonical; // Canonical URL
+    std::string robots; // Control indexing
+    std::string author; // Author of the content
+    std::string ogTitle; // Open Graph title
+    std::string ogDescription; // Open Graph description
+    std::string ogImage; // Open Graph image URL
+    std::string ogUrl; // Canonical URL for Open Graph
+    std::string twitterCard; // Twitter card type
+    std::string twitterTitle; // Twitter card title
+    std::string twitterDescription; // Twitter card description
+    std::string twitterImage; // URL for Twitter card image
+    std::string schema; // Schema.org markup
+    std::string charset = "UTF-8"; // Character set
+    std::string language; // Language code
+};
+
+
 struct Response {
 	std::string body;
 };
-
-// To Do:
-// maybe a class that have two functions one to send back a view as in html and other  in json
 
 
 class Resp
